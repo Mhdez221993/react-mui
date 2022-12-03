e2f8f799065b16e08146b45d28df1c9d67a1c721
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-03 21:23:43 +0100

    MUI Select with multiples options

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -2,20 +2,27 @@ import { Box, MenuItem, TextField } from "@mui/material";
 import React, { useState } from "react";
 
 const MuiSelect = () => {
-  const [country, setCountry] = useState("");
+  const [countries, setCountries] = useState<string[]>([]);
+  console.log(countries);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(e.target.value as string);
+    let value = e.target.value;
+    setCountries(typeof value === "string" ? value.split(",") : value);
   };
 
   return (
     <Box width="250px">
       <TextField
-        value={country}
+        value={countries}
         onChange={handleChange}
         label="Select Country"
         select
         fullWidth
+        SelectProps={{ multiple: true }}
+        color="secondary"
+        size="small"
+        helperText="Please select your country"
+        error
       >
         <MenuItem value="COL">Colombia</MenuItem>
         <MenuItem value="USA">USA</MenuItem>
