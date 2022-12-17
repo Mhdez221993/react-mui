052434c0338a56f68214695ce02f48ff80a81462
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-17 17:27:04 +0100

    Add varial to link component

--- src/components/MuiLink.tsx
+++ src/components/MuiLink.tsx
@@ -1,12 +1,19 @@
 import { Link, Stack } from "@mui/material";
 
+import Typography from "@mui/material/Typography";
+
 export const MuiLink = () => {
   return (
     <Stack direction="row" spacing={2} m={4}>
-      <Link href="#">Link</Link>
-      <Link href="#" color="secondary" underline="hover">
-        Secondary
+      <Link href="#" variant="body2">
+        Link
       </Link>
+
+      <Typography variant="h6">
+        <Link href="#" color="secondary" underline="hover">
+          Secondary
+        </Link>
+      </Typography>
     </Stack>
   );
 };
