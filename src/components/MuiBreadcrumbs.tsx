ca07b35423bad5489494dc82272f1b1aa024f0fc
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-17 17:53:40 +0100

    Add separator props to Breadcrambs component

--- src/components/MuiBreadcrumbs.tsx
+++ src/components/MuiBreadcrumbs.tsx
@@ -1,9 +1,11 @@
 import { Box, Breadcrumbs, Link, Typography } from "@mui/material";
 
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
 export const MuiBreadcrumbs = () => {
   return (
     <Box m={2}>
-      <Breadcrumbs aria-label="breadcrumbs">
+      <Breadcrumbs aria-label="breadcrumbs" separator={<NavigateNextIcon />}>
         <Link href="#" underline="hover">
           Home
         </Link>
