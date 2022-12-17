0d4bd24f3a1008a9327104ecbf9df4665f1bf48a
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-17 19:23:52 +0100

    Add src on Avatar component

--- src/components/MuiAvatar.tsx
+++ src/components/MuiAvatar.tsx
@@ -2,10 +2,20 @@ import { Avatar, Stack } from "@mui/material";
 
 export const MuiAvatar = () => {
   return (
-    <Stack>
+    <Stack spacing={2}>
       <Stack direction="row" spacing={1}>
-        <Avatar>BW</Avatar>
-        <Avatar>CK</Avatar>
+        <Avatar sx={{ backgroundColor: "primary.light" }}>BW</Avatar>
+        <Avatar sx={{ backgroundColor: "success.light" }}>CK</Avatar>
+      </Stack>
+
+      <Stack direction="row" spacing={1}>
+        <Avatar sx={{ backgroundColor: "primary.light" }}>BW</Avatar>
+        <Avatar sx={{ backgroundColor: "success.light" }}>CK</Avatar>
+
+        <Avatar src="https://randomuser.me/api/portraits/men/79.jpg">CK</Avatar>
+        <Avatar src="https://randomuser.me/api/portraits/women/79.jpg">
+          CK
+        </Avatar>
       </Stack>
     </Stack>
   );
