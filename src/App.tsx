786169d20d1cf887537972b1147e33ab496e7e48
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-17 17:22:11 +0100

    Add link with Mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
+import { MuiLink } from "./components/MuiLink";
 import { MuiNavbar } from "./components/MuiNavbar";
 
 function App() {
   return (
     <div className="App">
       <MuiNavbar />
+      <MuiLink />
     </div>
   );
 }
---
+++ src/components/MuiLink.tsx
@@ -0,0 +1,9 @@
+import { Link, Stack } from "@mui/material";
+
+export const MuiLink = () => {
+  return (
+    <Stack>
+      <Link href="#">Link</Link>
+    </Stack>
+  );
+};
