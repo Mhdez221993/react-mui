a83c27e4a352afe8887c46a695ccb1900d84b523
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-17 18:47:25 +0100

    Add props to Speed dial compoenent

--- src/components/MuiSpeedDial.tsx
+++ src/components/MuiSpeedDial.tsx
@@ -1,4 +1,4 @@
-import { FileCopyOutlined, Print, Share } from "@mui/icons-material";
+import { Edit, FileCopyOutlined, Print, Share } from "@mui/icons-material";
 import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
 
 export const MuiSpeedDial = () => {
@@ -6,11 +6,15 @@ export const MuiSpeedDial = () => {
     <SpeedDial
       ariaLabel="Navigation speed dial"
       sx={{ position: "absolute", bottom: 16, right: 16 }}
-      icon={<SpeedDialIcon />}
+      icon={<SpeedDialIcon openIcon={<Edit />} />}
     >
-      <SpeedDialAction icon={<FileCopyOutlined />} tooltipTitle="Copy" />
-      <SpeedDialAction icon={<Print />} tooltipTitle="Print" />
-      <SpeedDialAction icon={<Share />} tooltipTitle="Share" />
+      <SpeedDialAction
+        icon={<FileCopyOutlined />}
+        tooltipTitle="Copy"
+        tooltipOpen
+      />
+      <SpeedDialAction icon={<Print />} tooltipTitle="Print" tooltipOpen />
+      <SpeedDialAction icon={<Share />} tooltipTitle="Share" tooltipOpen />
     </SpeedDial>
   );
 };
