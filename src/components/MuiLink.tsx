fc461b3a179a9f16dfdf30780b638e644bb87237
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-17 17:23:38 +0100

    Add color prop with link Mui

--- src/components/MuiLink.tsx
+++ src/components/MuiLink.tsx
@@ -2,8 +2,11 @@ import { Link, Stack } from "@mui/material";
 
 export const MuiLink = () => {
   return (
-    <Stack>
+    <Stack direction="row" spacing={2} m={4}>
       <Link href="#">Link</Link>
+      <Link href="#" color="secondary">
+        Secondary
+      </Link>
     </Stack>
   );
 };
